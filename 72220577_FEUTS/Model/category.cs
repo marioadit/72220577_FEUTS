a98459e22ca2d501ad3ded49f125468814486c37
marioadit <marioadityakurnianto@gmail.com>
2024-10-28 13:59:36 +0700

    Add project files.

---
+++ 72220577_FEUTS/Model/category.cs
@@ -0,0 +1,11 @@
+﻿namespace _72220577_FEUTS.Model
+{
+    public class category
+    {
+        [ConfigurationKeyName("categoryId")]
+        public int categoryId { get; set; }
+
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ 72220577_FEUTS/Model/course.cs
@@ -0,0 +1,16 @@
+﻿namespace _72220577_FEUTS.Model
+{
+    public class course
+    {
+        [ConfigurationKeyName("courseId")]
+        public int courseId { get; set; }
+        public string name { get; set; }
+        public string imageName { get; set; }
+        public int duration { get; set; }
+        public string description { get; set; }
+
+        // Foreign Key to Categories
+        public category category { get; set; }
+
+    }
+}
